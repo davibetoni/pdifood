ab56797f9bda4d68b33666bf06ecbb3ae9138f37
davibetoni <davibetoni@gmail.com>
2023-01-19 16:51:22 -0300

    remove dist

--- dist/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolvers = void 0;
-const getRestaurantsResolver_1 = require("./getRestaurantsResolver");
-exports.resolvers = {
-    Query: {
-        restaurants: getRestaurantsResolver_1.getRestaurantsResolver,
-    },
-};
--- dist/services/restaurants/getRestaurantsService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GetRestaurantsService = void 0;
-const restaurant_1 = require("../../entities/restaurant");
-async function GetRestaurantsService() {
-    return await restaurant_1.Restaurant.findAll();
-}
-exports.GetRestaurantsService = GetRestaurantsService;
